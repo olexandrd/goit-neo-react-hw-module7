feabb13e5345d4d77a4b2d1001abbf8a2ed55b06
Olexandrd <olexandrd@gmail.com>
2024-09-03 21:53:40 +0300

    contacts loading from app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
 import css from "./App.module.css";
+import {
+  selectError,
+  selectFilteredContacts,
+  selectIsLoading,
+} from "./redux/selectors";
+import { fetchContacts } from "./redux/contactsOps";
+import Loader from "./components/Loader/Loader";
+import LoadError from "./components/LoadError/LoadError";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const filteredContacts = useSelector(selectFilteredContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={css.app}>
       <h1 className={css.header}>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList />
+      {filteredContacts.length > 0 ? (
+        <ContactList />
+      ) : (
+        !(loading || !!error) && <p>No contacts found</p>
+      )}
+      {loading && <Loader />}
+      {error && <LoadError />}
     </div>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,39 +1,20 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import css from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
-import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contactsOps";
-import {
-  selectError,
-  selectFilteredContacts,
-  selectIsLoading,
-} from "../../redux/selectors";
-import Loader from "../Loader/Loader";
-import LoadError from "../LoadError/LoadError";
+import { selectFilteredContacts } from "../../redux/selectors";
 
 const ContactList = () => {
-  const dispatch = useDispatch();
-  const loading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
   const filteredContacts = useSelector(selectFilteredContacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
   return (
     <>
       <ul className={css.contactList}>
-        {filteredContacts.length > 0
-          ? filteredContacts.map(({ id, name, number }) => (
-              <li key={id}>
-                <Contact name={name} number={number} id={id} />
-              </li>
-            ))
-          : !loading && <p>No contacts found</p>}
+        {filteredContacts.map(({ id, name, number }) => (
+          <li key={id}>
+            <Contact name={name} number={number} id={id} />
+          </li>
+        ))}
       </ul>
-      {loading && <Loader />}
-      {error && <LoadError />}
     </>
   );
 };
